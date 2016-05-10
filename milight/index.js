fa0bf45af74a025ab1ebece2ba64fcc46952a5e0
HJ Cross <github@chimpjuice.com>
2016-05-09 22:40:25 -0400

    remove const, use var

--- milight/index.js
+++ milight/index.js
@@ -3,7 +3,7 @@ var milight = require('./milight');
 var Hapi = require('hapi');
 
 // Create a server with a host and port
-const server = new Hapi.Server();
+var server = new Hapi.Server();
 server.connection({ 
     host: '0.0.0.0', 
     port: parseInt(process.env.PORT, 10) || 3000
