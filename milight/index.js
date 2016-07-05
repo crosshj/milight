743333333bf255ffc01e0ff19fe82b1d7d849282
crosshj <github@chimpjuice.com>
2016-07-05 00:01:00 -0400

    add another box - this will need abstracting later

--- milight/index.js
+++ milight/index.js
@@ -4,26 +4,51 @@ var Hapi = require('hapi');
 
 // Create a server with a host and port
 const server = new Hapi.Server();
-server.connection({ 
-    host: '0.0.0.0', 
+server.connection({
+    host: '0.0.0.0',
     port: parseInt(process.env.PORT, 10) || 3000
 });
 
 server.route({
-  method: 'GET'
-, path: '/milights/{switch}'
-, handler: function(req, reply) {
+  method: 'GET',
+  path: '/milights/{switch}',
+  handler: function(req, reply) {
     var lightSwitch = req.params.switch || 'off';
-    console.log(req.params);
+    console.log("Den Server - ",req.params);
     milight[lightSwitch]();
     reply('ok');
   }
 });
 
-// Start the server
 server.start(function(err) {
-    if (err) {
-        throw err;
+    if (err) { throw err; }
+    console.log('\n--- Den Server running at:', server.info.uri);
+});
+
+
+
+const masterBServer = new Hapi.Server();
+masterBServer.connection({
+    host: '0.0.0.0',
+    port: 3001
+});
+
+masterBServer.route({
+  method: 'GET',
+  path: '/milights/{zone}/{switch}',
+  handler: function(req, reply) {
+    var lightSwitch = "zone" + req.params.zone + (req.params.switch || 'off');
+    console.log("Master Bedroom Server - ", req.params);
+    if ([1,2,3,4].includes(Number(req.params.zone))){
+      milight[lightSwitch]();
+      reply('ok');
+    } else {
+      reply('error: zone must be 1,2,3,4')
     }
-    console.log('Server running at:', server.info.uri);
+  }
+});
+
+masterBServer.start(function(err) {
+    if (err) { throw err; }
+    console.log('--- Master Bedroom Server running at:', masterBServer.info.uri);
 });
--- milight/milight.js
+++ milight/milight.js
@@ -3,16 +3,33 @@ var WifiBoxModule = require('./wifibox.js');
 var cmd = require('./commands.js');
 
 var box = new WifiBoxModule("192.168.1.56", 8899);
+var masterBox = new WifiBoxModule("192.168.1.4", 8899);
 
-function off() {
+function off(zone) {
+  if (zone) {
+    masterBox.command(cmd.rgbw.off(zone));
+  } else {
     box.command(cmd.rgb.off());
+  }
 }
 
-function on() {
-    box.command(cmd.rgb.on());
+function on(zone) {
+    if (zone) {
+      masterBox.command(cmd.rgbw.on(zone));
+    } else {
+      box.command(cmd.rgb.on());
+    }
 }
 
-module.exports = { 
+module.exports = {
   on: on,
-  off: off
+  off: off,
+  zone1on: on.bind(null,1),
+  zone1off: off.bind(null,1),
+  zone2on: on.bind(null,2),
+  zone2off: off.bind(null,2),
+  zone3on: on.bind(null,3),
+  zone3off: off.bind(null,3),
+  zone4on: on.bind(null,4),
+  zone4off: off.bind(null,4)
 }
